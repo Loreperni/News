1ca8b7dbd37a42b7d47fce3bf583c276ac1b9ab5
Loreperni <Lorenzo.perniciano@bywaves.it>
2024-04-28 16:45:46 +0200

    test

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+// api.service.spec.ts
 
-import { APIService } from './api.service';
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { APIService } from "./api.service";
 
-describe('APIService', () => {
+describe("APIService", () => {
   let service: APIService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [APIService],
+    });
     service = TestBed.inject(APIService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
+
+  it("should get latest news IDs", () => {
+    const dummyIDs: number[] = [1, 2, 3];
+    service.getLatestNewsIDs().subscribe((ids) => {
+      expect(ids).toEqual(dummyIDs);
+    });
+
+    const req = httpMock.expectOne(
+      "https://hacker-news.firebaseio.com/v0/newstories.json"
+    );
+    expect(req.request.method).toBe("GET");
+    req.flush(dummyIDs);
+  });
+
+  // Add more tests for getNewsDetail(id) method
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,21 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// home.component.spec.ts
 
-import { HomeComponent } from './home.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HomeComponent } from "./home.component";
+import { APIService } from "../api.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-describe('HomeComponent', () => {
+describe("HomeComponent", () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      providers: [APIService],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should initialize newsList, currentPage, and newsPerPage", () => {
+    expect(component.newsList).toEqual([]);
+    expect(component.currentPage).toBe(0);
+    expect(component.newsPerPage).toBe(10);
+  });
+
+  it("should call loadNews() on component initialization", () => {
+    spyOn(component, "loadNews");
+    component.ngOnInit();
+    expect(component.loadNews).toHaveBeenCalled();
+  });
+
+  // Add more tests for loadNews() and onLoadMore() methods
 });
